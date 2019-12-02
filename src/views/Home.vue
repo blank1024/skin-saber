171ca1f8f40af1b1bb284379b4fc91cbace49c5f
jawnwa22 <jawnwa22@gmail.com>
2019-12-02 23:59:33 +0800

    feat(Home):新增首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+    article.article
+        div.article__info
+            div.article__info--left
+                router-link(to="/").author
+                    img.author__avatar(src="~@/assets/images/blogger_avatar.jpeg")
+                    span.author__name jawnwa22
+                span.split in
+                span.tags
+                    router-link(to="/") 1
+                    router-link(to="/") 1
+            div.article__info--right
+                span.createTime 2018-08-22
+        div.article__intro
+        div.article__visit
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    components: {}
+};
 </script>
+
+<style lang="scss" scoped>
+.article {
+}
+.article__info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .article__info--left {
+        @include vc;
+    }
+    .author {
+        @include vc;
+        color: #333;
+    }
+    .author__avatar {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+    }
+    .author__name {
+        margin-left: 10px;
+        flex: auto;
+    }
+    .split {
+        padding: 0 8px;
+        flex: auto;
+    }
+    .tags {
+        color: #333;
+        a {
+            color: inherit;
+        }
+    }
+}
+</style>
